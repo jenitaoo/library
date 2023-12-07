95548b3b440248fecfa5a2e8e69b730c24af7c67
Jenny Thao Huynh C22448184 <c22448184@mytudublin.ie>
2023-12-07 19:50:16 +0000

    hid view page

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,6 @@
 <header>
     <a href='search.php'>Search Books</a>
     <a href='reserve.php'>Reserve Books</a>
-    <a href='view.php'>View Books</a>
     <a href='view_reserved.php'>View Your Reserved Books</a>
     <a href='logout.php'>Log Out</a>
 </header>
