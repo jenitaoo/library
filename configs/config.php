2b4b77383f541e2ccd58195384072441976f6177
Jenny Thao Huynh C22448184 <c22448184@mytudublin.ie>
2023-12-08 01:56:53 +0000

    small tweaks

--- configs/config.php
+++ configs/config.php
@@ -18,7 +18,7 @@ $conn = new mysqli($servername, $username, $password, $dbname); // create a new 
 if ($conn->connect_error) { // access error property of conn, if there's sumn there (a string describing the error)
     die("Connection failed: " . $conn->connect_error); // terminate script and display message
 }
-echo "Connected To Database Successfully"
+//echo "Connected To Database Successfully"
 
 //$conn->close(); // good practice to close your db connection
 ?>
